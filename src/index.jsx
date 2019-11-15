ed319328b0da4999ca36b43db69f3d6a57dbcabb
code_shoulder <arban.nichs@gmail.com>
2019-11-15 01:26:16 -0500

    mongo done

--- src/index.jsx
+++ src/index.jsx
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './jsx/App';
-import Loading from './jsx/components/common/Loading';
 import { Provider } from './jsx/components/common/context';
 
 ReactDOM.render(
   <Router>
-    <React.Suspense fallback={<Loading />}>
-      <Provider>
-        <App />
-      </Provider>
-    </React.Suspense>
+    <Provider>
+      <App />
+    </Provider>
   </Router>,
   document.getElementById('root'),
 );
--- src/jsx/components/common/Loading.jsx
+++ src/jsx/components/common/Loading.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Card, CardImg } from 'react-bootstrap';
+import logo from '../../../static/android-chrome-512x512.png';
 
 function Loading() {
     return (
         <>
             <Card>
-                <CardImg variant="top" src="../static/android-chrome-512x512.png" />
+                <CardImg variant='top' src={logo} />
             </Card>
         </>
     )
